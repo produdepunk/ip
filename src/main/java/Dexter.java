eef46306b37a73a33be0968e83d82f1014c525a9
yuhui <yuhuiheu@gmail.com>
2026-08-19 00:05:46 +0800

    feat: changed the welcome message and added a goodbye message

--- src/main/java/Dexter.java
+++ src/main/java/Dexter.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class Dexter {
     public static void main(String[] args) {
         String banner = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +6,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(banner);
+
+        System.out.println("Welcome my fellow big-brainers! What questions do you have in mind?");
+        System.out.println("See you again soon!");
     }
 }
