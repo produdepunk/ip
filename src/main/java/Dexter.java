b1c8dd4ce130661456b821d082f30c3e1071e0a3
yuhui <yuhuiheu@gmail.com>
2026-08-19 00:16:41 +0800

    feat: changed the banner and completed level-0

--- src/main/java/Dexter.java
+++ src/main/java/Dexter.java
@@ -1,13 +1,13 @@
 public class Dexter {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String banner = "DDDD   EEEEE  XX XX  TTTTT  EEEEE  RRRR\n"
+                + "D   D  E       X X     T    E      R   R\n"
+                + "D   D  EEEE     X      T    EEEE   RRRR\n"
+                + "D   D  E        X      T    E      R R\n"
+                + "DDDD   EEEEE   X X     T    EEEEE  R  RR\n";
         System.out.println(banner);
 
-        System.out.println("Welcome my fellow big-brainers! What questions do you have in mind?");
+        System.out.println("Welcome my fellow big-brainer! What question do you have in mind?");
         System.out.println("See you again soon!");
     }
 }
