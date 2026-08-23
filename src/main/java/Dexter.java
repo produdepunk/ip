9b826db093e64aae47afc7dc18fbc95f82958640
yuhui <yuhuiheu@gmail.com>
2026-08-23 12:02:53 +0800

    feat: added the mark and unmark command

--- src/main/java/Dexter.java
+++ src/main/java/Dexter.java
@@ -11,22 +11,49 @@ public class Dexter {
 
         System.out.println("Welcome my fellow big-brainer! What question do you have in mind?");
 
-
         String line = "";
         String[] itemList = new String[100];
         int itemCount = 0;
+        String[] completionStatus = new String[100];
 
         while (!line.equals("bye")) {
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
             if (line.equals("list")) {
 
+                System.out.println("Here is your list of tasks!");
                 for (int i = 0; i < itemCount; i++) {
-                    System.out.println(i + 1 + ". " + itemList[i]);
+                    System.out.println(i + 1 + ".[" + completionStatus[i] + "] " + itemList[i]);
                 }
+            } else if (line.indexOf("mark") == 0) {
+                String numberString = "";
+                int index = 4;
+                while (index < line.length()) {
+                    if (line.charAt(index) >= '0' && line.charAt(index) < '9') {
+                        numberString += line.charAt(index);
+                    }
+                    index++;
+                }
+                int numberInt = Integer.parseInt(numberString);
+                completionStatus[numberInt - 1] = "X";
+                System.out.println("Alright! Marked it as done!");
+            } else if (line.indexOf("Unmark") == 0) {
+                String numberString = "";
+                int index = 4;
+                while (index < line.length()) {
+                    if (line.charAt(index) >= '0' && line.charAt(index) < '9') {
+                        numberString += line.charAt(index);
+                    }
+                    index++;
+                }
+                int numberInt = Integer.parseInt(numberString);
+                completionStatus[numberInt - 1] = " ";
+                System.out.println("Alright! I have unchecked the task!");
             } else if (!line.equals("bye")) {
-                itemList[itemCount++] = line;
-                System.out.println("added: " + line + "\n");
+                itemList[itemCount] = line;
+                completionStatus[itemCount] = " ";
+                itemCount++;
+                System.out.println("added: " + line);
             }
         }
 
